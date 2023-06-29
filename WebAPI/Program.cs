9aee5d4b6adf1d2847e3582a2ff874623dc2848a
Senol <senoltr@yandex.com>
2023-06-30 01:33:31 +0300

    The work engine and image-adding services are integrated.

--- WebAPI/Program.cs
+++ WebAPI/Program.cs
@@ -1,11 +1,15 @@
-
 using Autofac;
 using Autofac.Extensions.DependencyInjection;
-using Business.Abstract;
-using Business.Concrete;
+using Business.DependencyResolvers;
 using Business.DependencyResolvers.Autofac;
-using DataAccess.Abstract;
-using DataAccess.Concrete.EntityFramework;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.Hosting;
+using Microsoft.Extensions.Logging;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
 
 namespace WebAPI
 {
@@ -13,63 +17,19 @@ namespace WebAPI
     {
         public static void Main(string[] args)
         {
-            var builder = WebApplication.CreateBuilder(args);
+            CreateHostBuilder(args).Build().Run();
+        }
 
-            // Add services to the container.
-
-            builder.Services.AddControllers();
-            // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
-            builder.Services.AddEndpointsApiExplorer();
-            builder.Services.AddSwaggerGen();
-
-
-            //builder.Services.AddSingleton<IBrandService, BrandManager>();
-            //builder.Services.AddSingleton<IBrandDal, EfBrandDal>();
-
-            //builder.Services.AddSingleton<ICarService, CarManager>();
-            //builder.Services.AddSingleton<ICarDal, EfCarDal>();
-
-            //builder.Services.AddSingleton<IColorService, ColorManager>();
-            //builder.Services.AddSingleton<IColorDal, EfColorDal>();
-
-            //builder.Services.AddSingleton<ICustomerService, CustomerManager>();
-            //builder.Services.AddSingleton<ICustomerDal, EfCustomerDal>();
-
-            //builder.Services.AddSingleton<IRentalService, RentalManager>();
-            //builder.Services.AddSingleton<IRentalDal, EfRentalDal>();
-
-            //builder.Services.AddSingleton<IUserService, UserManager>();
-            //builder.Services.AddSingleton<IUserDal, EfUserDal>();
-
-            builder.Host.UseServiceProviderFactory(new AutofacServiceProviderFactory())
+        public static IHostBuilder CreateHostBuilder(string[] args) =>
+            Host.CreateDefaultBuilder(args)
+                .UseServiceProviderFactory(new AutofacServiceProviderFactory())
                 .ConfigureContainer<ContainerBuilder>(builder =>
                 {
                     builder.RegisterModule(new AutofacBusinessModule());
+                })
+                .ConfigureWebHostDefaults(webBuilder =>
+                {
+                    webBuilder.UseStartup<Startup>();
                 });
-
-
-
-
-
-            var app = builder.Build();
-
-
-
-            // Configure the HTTP request pipeline.
-            if (app.Environment.IsDevelopment())
-            {
-                app.UseSwagger();
-                app.UseSwaggerUI();
-            }
-
-            app.UseHttpsRedirection();
-
-            app.UseAuthorization();
-
-
-            app.MapControllers();
-
-            app.Run();
-        }
     }
 }
